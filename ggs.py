d24c61247d381fdc1ea0d7e89cdf052c558b8829
Pavanpawar2705 <pavanpawar2705@gmail.com>
2021-02-17 12:21:00 +0530

    properly updated os code

--- ggs.py
+++ ggs.py
@@ -1,2 +1,7 @@
 import os
-print('This is present directory---->',os.getcwd())
+print('Hi, How can i help you?')
+s=input()
+if s=='getcwd':
+    print('This is present directory---->',os.getcwd())
+else:
+    print('Service Unavailable')
